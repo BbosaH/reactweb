a1f83da5d3b6580bc2cb9e698b61ac3fa47e34e5
Henry Bbosa <bbosa.henry1@gmail.com>
2017-10-12 05:00:15 +0300

    included testdirectory

---
+++ test/actions/actions.test.jsx
@@ -0,0 +1,9 @@
+/**
+ * @Author: Henry Bbosa
+ * @Date:   2017-10-12T04:59:16+03:00
+ * @Email:  bbosa.henry1@gmail.com
+ * @Project: potato
+ * @Filename: actions.test.jsx
+ * @Last modified by:   Henry Bbosa
+ * @Last modified time: 2017-10-12T04:59:19+03:00
+ */
