56e64ea681799da53c0fb96a7d1dc5277666a0c1
Henry Bbosa <bbosa.henry1@gmail.com>
2017-10-22 18:32:42 +0300

    edited searchbox regex searchbox works better

--- app/actions/search-box-actions.jsx
+++ app/actions/search-box-actions.jsx
@@ -6,7 +6,7 @@ export const changeSearchTextAction=(search_text,full_articles)=>{
 
     return(dispatch)=>{
 
-        const pattern = new RegExp(search_text);
+        const pattern = new RegExp(search_text,"ig");
         const articles = full_articles.filter((article)=>{
             return pattern.test(article.body_text) 
             || pattern.test(article.title) || pattern.test(article.topic.name)
